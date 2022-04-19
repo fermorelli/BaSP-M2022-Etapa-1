5cef354d4ee120069a7c4ca26329f93b4a0e9bf5
ilcosme <fer.c@live.com.ar>
2022-04-19 10:53:01 -0300

    BaSP-W05-M2022: ex-06-functions, changes to language

--- Semana-05/Exercises/06_functions.js
+++ Semana-05/Exercises/06_functions.js
@@ -4,19 +4,17 @@ console.log('--EXERCISE 6: FUNCTIONS');
 
 console.log('-Exercise 6.a:');
 
-function suma(num1, num2){
+function sum(num1, num2){
     return num2+num1;
 };
 
-var resultado = suma(4,6);
-
-console.log('Resultado:', resultado);
+console.log('Result:', sum(4,6));
 
 //b. A la función suma anterior, agregarle una validación para controlar si alguno de los parámetros no es un número, mostrar una alerta aclarando que uno de los parámetros tiene error y retornar el valor NaN como resultado.
 
 console.log('-Exercise 6.b:');
 
-function suma(num1, num2){
+function sum(num1, num2){
     if (typeof num1 && typeof num2 === 'number'){
         return num1+num2;
     } else{
@@ -25,9 +23,7 @@ function suma(num1, num2){
     }  
 };
 
-var resultado1 = suma(10, 'dos');
-
-console.log('Resultado:', resultado1);
+console.log('Result:', sum(10, 'dos'));
 
 //c. Crear una función validate integer que reciba un número como parámetro y devuelva verdadero si es un número entero.
 
@@ -37,13 +33,13 @@ function validateInteger(number){
     return (number === Math.floor(number));
 }
 
-console.log('Resultado:', validateInteger(8), validateInteger(3.141516));
+console.log('Result:', validateInteger(8), validateInteger(3.141516));
 
 //d. A la función suma del ejercicio 6b) agregarle una llamada que valide que los números sean enteros. En caso que haya decimales mostrar un alerta con el error y retorna el número convertido a entero (redondeado).
 
 console.log('-Exercise 6.d:');
 
-function sumaDos(num1, num2){
+function sumTwo(num1, num2){
     if (typeof num1 != 'number' || typeof num2 != 'number'){
         alert('One of the parameters is not a number')
         return NaN
@@ -55,13 +51,13 @@ function sumaDos(num1, num2){
     return num1+num2;
 };
 
-console.log('Resultado:', sumaDos(100.456,20.4567));
+console.log('Result:', sumTwo(100.456,20.4567));
 
 //e. Convertir la validación del ejercicio 6d) en una función separada y llamarla dentro de la función suma probando que todo siga funcionando igual.
 
 console.log('-Exercise 6.e:')
 
-function validacion(num){
+function validation(num){
     if (!validateInteger(num)){
         alert('Number is not an integer')
         num = Math.round(num);
@@ -69,14 +65,14 @@ function validacion(num){
     return num;
 };
 
-function sumaTres(num1, num2){
+function sumThree(num1, num2){
     if (typeof num1 != 'number' || typeof num2 != 'number'){
         alert('One of the parameters is not a number')
         return NaN;
     }
-    num1 = validacion(num1);
-    num2 = validacion(num2);
+    num1 = validation(num1);
+    num2 = validation(num2);
     return num1+num2;
 }
 
-console.log('Resultado:', sumaTres(80,90.657));
+console.log('Result:', sumThree(80,90.657));
