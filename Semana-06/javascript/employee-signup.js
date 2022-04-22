41b52c47591710f860e1c9d48e7fc007dbc64472
ilcosme <fer.c@live.com.ar>
2022-04-22 19:17:34 -0300

    BaSP-W06-M2022: finished modal code, all working fine, lacking focus event

--- Semana-06/javascript/employee-signup.js
+++ Semana-06/javascript/employee-signup.js
@@ -1,35 +1,49 @@
 var nameForm = document.querySelector('#name');
 var alertName = document.querySelector('.name-alert')
+var nameChecked = document.getElementById('name-checked');
 
 var surname = document.querySelector('#surname');
 var alertSurname = document.querySelector('.surname-alert');
+var surnameChecked = document.getElementById('surname-checked')
 
 var dni = document.querySelector('#dni');
 var alertDni = document.querySelector('.dni-alert');
+var dniChecked = document.getElementById('dni-checked')
 
 var dob = document.querySelector('#dob');
 var alertDob = document.querySelector('.dob-alert');
+var dobChecked = document.getElementById('dob-checked')
 
 var phone = document.querySelector('#phone');
 var alertPhone = document.querySelector('.phone-alert');
+var phoneChecked = document.getElementById('phone-checked')
 
 var adress = document.querySelector('#adress');
 var alertAdress = document.querySelector('.adress-alert');
+var adressChecked = document.getElementById('adress-checked')
 
 var city = document.querySelector('#city');
-var alertCity = document.querySelector('.city-alert')
+var alertCity = document.querySelector('.city-alert');
+var cityChecked = document.getElementById('city-checked')
 
 var zipCode = document.querySelector('#zip-code');
 var alertZipCode = document.querySelector('.zip-code-alert');
+var zipCodeChecked = document.getElementById('zip-code-checked')
 
 var mail = document.querySelector('#mail');
 var alertMail = document.querySelector('.mail-alert');
+var mailChecked = document.getElementById('mail-checked');
 
 var password = document.querySelector('#password');
 var alertPass = document.querySelector('.password-alert');
+var passChecked = document.getElementById('pass-checked');
 
 var rePassword = document.querySelector('#re-password');
 var alertRePass = document.querySelector('.re-password-alert');
+var rePassChecked = document.getElementById('re-pass-checked')
+
+var button = document.getElementById('su-button');
+var button2 = document.getElementById('button-7');
 
 //NAME VALIDATION
 
@@ -272,4 +286,92 @@ rePassword.addEventListener('blur', function(){
         alertRePass.style.color = 'red';
         alertRePass.innerHTML = '<span>You must enter a valid password format</span>';
     }
+});
+
+//MODAL CODE
+
+button.addEventListener('click', function(){
+    var nameValue = nameForm.value;
+    var surnameValue = surname.value;
+    var dniValue = dni.value;
+    var dobValue = dob.value;
+    var phoneValue = phone.value;
+    var adressValue = adress.value;
+    var cityValue = city.value;
+    var zipCodeValue = zipCode.value;
+    var mailValue = mail.value;
+    var passValue = password.value;
+    var rePassValue = rePassword.value;
+
+    document.getElementById('overlay').style.display = 'block';
+
+    if(nameForm.classList.contains('validation')){
+        nameChecked.innerHTML = '<span>Name: </span>' + nameValue;
+    }else if(nameForm.classList.contains('negation')){
+        nameChecked.innerHTML = '<span>Name: Incorrect name format</span>';
+    };
+
+    if(surname.classList.contains('validation')){
+        surnameChecked.innerHTML = '<span>Surname: </span>' + surnameValue;
+    }else if(surname.classList.contains('negation')){
+        surnameChecked.innerHTML = '<span>Surname: Incorrect surname format</span>';
+    };
+
+    if(dni.classList.contains('validation')){
+        dniChecked.innerHTML = '<span>DNI: </span>' + dniValue;
+    }else if(dni.classList.contains('negation')){
+        dniChecked.innerHTML = '<span>DNI: Incorrect DNI format</span>';
+    };
+
+    if(dob.classList.contains('validation')){
+        dobChecked.innerHTML = '<span>Date of birth: </span>' + dobValue;
+    }else if(dob.classList.contains('negation')){
+        dobChecked.innerHTML = '<span>Date of birth: Incorrect date format</span>';
+    };
+
+    if(phone.classList.contains('validation')){
+        phoneChecked.innerHTML = '<span>Phone: </span>' + phoneValue;
+    }else if(phone.classList.contains('negation')){
+        phoneChecked.innerHTML = '<span>Phone: Incorrect phone format</span>';
+    };
+
+    if(adress.classList.contains('validation')){
+        adressChecked.innerHTML = '<span>Adress: </span>' + adressValue;
+    }else if(adress.classList.contains('negation')){
+        adressChecked.innerHTML = '<span>Adress: Incorrect adress format</span>';
+    };
+
+    if(city.classList.contains('validation')){
+        cityChecked.innerHTML = '<span>City: </span>' + cityValue;
+    }else if(city.classList.contains('negation')){
+        cityChecked.innerHTML = '<span>City: Incorrect city format</span>';
+    };
+
+    if(zipCode.classList.contains('validation')){
+        zipCodeChecked.innerHTML = '<span>Zip code: </span>' + zipCodeValue;
+    }else if(zipCode.classList.contains('negation')){
+        zipCodeChecked.innerHTML = '<span>Zip code: Incorrect zip code format</span>';
+    };
+
+    if(mail.classList.contains('validation')){
+        mailChecked.innerHTML = '<span>Mail: </span>' + mailValue;
+    }else if(mail.classList.contains('negation')){
+        mailChecked.innerHTML = '<span>Mail: Incorrect mail format</span>';
+    };
+
+    if(password.classList.contains('validation')){
+        passChecked.innerHTML = '<span>Password: </span>' + passValue;
+    }else if(password.classList.contains('negation')){
+        passChecked.innerHTML = '<span>Password: Incorrect password format</span>';
+    };
+
+    if(rePassword.classList.contains('validation')){
+        rePassChecked.innerHTML = '<span>Repeat password: </span>' + rePassValue;
+    }else if(rePassword.classList.contains('negation')){
+        rePassChecked.innerHTML = '<span>Repeat password: Incorrect password format</span>';
+    };
+});
+
+button2.addEventListener('click', function(){
+    document.getElementById('overlay').style.display = 'none';
 });
